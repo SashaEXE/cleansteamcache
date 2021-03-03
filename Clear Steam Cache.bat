cc577d38b9610648fbd77c3ecc74f02dbf838592
SashaEXE <66440642+SashaEXE@users.noreply.github.com>
2021-03-03 19:15:53 +0100

    Add files via upload

---
+++ Clear Steam Cache.bat
@@ -0,0 +1,12 @@
+@echo off
+taskkill /f /IM steamwebhelper.exe
+taskkill /f /IM steam.exe
+rd /s /q "C:\Program Files (x86)\Steam\steam\cached"
+rd /s /q "C:\Program Files (x86)\Steam\appcache"
+rd /s /q "C:\Program Files (x86)\Steam\depotcache"
+rd /s /q "C:\Program Files (x86)\Steam\steamapps\shadercache"
+rd /s /q "C:\Program Files (x86)\Steam\*cache
+del /s /q "C:\Program Files (x86)\Steam\*.cachedmsg"
+del /s /q "C:\Program Files (x86)\Steam\*.vdf"
+del /s /q "C:\Program Files (x86)\Steam\*socache.dt"
+pause
